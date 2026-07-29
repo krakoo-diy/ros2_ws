cccd272b4fbdf17ec39a5e38aab82906ea2bacb4
Krakoo <2foox2gamer@gmail.com>
2026-07-29 23:39:32 +0000

    new world added, launch file still neeeds to be fixed!

--- install/oppressor_mk3_description/share/oppressor_mk3_description/launch/gazebo.launch.py
+++ install/oppressor_mk3_description/share/oppressor_mk3_description/launch/gazebo.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
    
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")),
-        launch_arguments={"gz_args": "-r -v 4 empty.sdf"}.items()
+        launch_arguments={"gz_args": "-r -v 4 " + os.path.join(get_package_share_directory('oppressor_mk3_description'), 'worlds', 'custom_world.sdf')}.items()
     )
 
     spawn_robot = TimerAction(
